a032eab43a9975dfb066b502277ba15f3870f13c
Сергей Емельянов <SYemelianau@gomel.iba.by>
2020-11-30 22:43:17 +0300

    ang2-adv - Validation - task4.1
    Change-Id: I5926c54c5a513559489b7a12716da7f1376f2470

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  registrationForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    name: new FormControl('', [Validators.required]),
+    agree: new FormControl('', [Validators.required, Validators.requiredTrue])
+  });
 
   constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onSubmit(): void {}
 }
