1d0d0e1a8096a0eecdaa11457617130bb8483aeb
Сергей Емельянов <SYemelianau@gomel.iba.by>
2021-03-26 23:09:09 +0300

    ang2-adv - task15
    Change-Id: Ic131f56e5247380fe8ae00101c0888ad0442cd7c

---
+++ src/app/not-found/not-found.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-not-found',
+    templateUrl: './not-found.component.html',
+    styleUrls: ['../auth/auth.component.css', './not-found.component.css']
+})
+export class NotFoundComponent {}
